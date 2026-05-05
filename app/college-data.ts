3f9621caf7e47b0f0a2d181924610e21ada6ed09
IMMU2505 <imdadullashaik123456@gmail.com>
2026-05-05 17:03:36 +0530

    Create college-data.ts

---
+++ app/college-data.ts
@@ -0,0 +1,12 @@
+export const COLLEGE_INFO = `
+COLLEGE: Audisankara College Of engineering and technology, Gudur
+COURSES: B.Tech CSE - 1.05 Lakh/year, 180 seats | ECE - 1.05 Lakh/year, 120 seats
+FEES: Tuition 1,05,000/year, Hostel 60,000/year
+PLACEMENTS 2024: Highest 12 LPA Amazon, Average 4.2 LPA
+TOP RECRUITERS: TCS, Infosys, Wipro, Accenture, Capgemini
+FACILITIES: Central Library 50k books, WiFi Campus, Boys/Girls Hostel, Sports Complex
+CAMPUS LIFE: Technical fests, NSS, IEEE Student Branch, Cultural Club
+INTERNSHIPS: TCS iON 2-month internship for 3rd year students
+CONTACT: 08565-248990, info@aitsrajampet.edu.in
+ADDRESS: NH16, Gudur, Andhra Pradesh 524101
+`
